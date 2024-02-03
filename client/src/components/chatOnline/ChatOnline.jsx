475381d25fce7fbf732a7e0e7802504e55317b43
Jitesh Shewaramani <raj7000521814@gmail.com>
2024-02-03 10:19:34 +0530

    Revert "Revert "Responsive Designs""
    This reverts commit fa5e10cc7381c74e060b69465d96d631c0764744.

--- client/src/components/chatOnline/ChatOnline.jsx
+++ client/src/components/chatOnline/ChatOnline.jsx
@@ -1,15 +1,15 @@
-import React, { useEffect,useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import "./chatOnline.css"
 import axios from 'axios';
 
-const ChatOnline = ({onlineUsers,currentId,setCurrentChat}) => {
+const ChatOnline = ({ onlineUsers, currentId, setCurrentChat }) => {
 
   const [friends, setFriends] = useState([]);
   const [onlineFriends, setOnlineFriends] = useState([]);
 
   useEffect(() => {
     const getFriends = async () => {
-      const res = await axios.get(process.env.REACT_APP_BACKEND+"/api/users/friends/" + currentId);
+      const res = await axios.get(process.env.REACT_APP_BACKEND + "/api/users/friends/" + currentId);
       setFriends(res.data);
     };
 
@@ -24,7 +24,7 @@ const ChatOnline = ({onlineUsers,currentId,setCurrentChat}) => {
 
   const handleClick = async (user) => {
     try {
-      const res = await axios.get(process.env.REACT_APP_BACKEND+
+      const res = await axios.get(process.env.REACT_APP_BACKEND +
         `/api/conversation/find/${currentId}/${user._id}`
       );
       setCurrentChat(res.data);
@@ -33,25 +33,32 @@ const ChatOnline = ({onlineUsers,currentId,setCurrentChat}) => {
     }
   };
 
-    const PF=process.env.REACT_APP_PUBLIC_FOLDER;
+  const PF = process.env.REACT_APP_PUBLIC_FOLDER;
   return (
     
-      <div className="chatOnline">
-        {onlineFriends.map((online_friend)=>(
+    onlineFriends.length>0?
+  (    <div className="chatOnline">
+      {onlineFriends.map((online_friend) => (
 
-          <div className="chatOnlineFriend" onClick={()=>handleClick(online_friend)}>
-            <div className="chatOnlineImgContainer">
-                <img className="chatOnlineImg"
-                src={online_friend?.profilePicture
-                  ? PF + online_friend.profilePicture
-                  : PF + "/avatar.jpg" }alt=""></img>
-                <div className="chatOnlineBadge"></div>
-            </div>
-        <span className="chatOnlineName">{online_friend.username} </span>
+        <div className="chatOnlineFriend" onClick={() => handleClick(online_friend)}>
+          <div className="chatOnlineImgContainer">
+            <img className="chatOnlineImg"
+              src={online_friend?.profilePicture
+                ? PF + online_friend.profilePicture
+                : PF + "/avatar.jpg"} alt=""></img>
+            <div className="chatOnlineBadge"></div>
+          </div>
+          <span className="chatOnlineName">{online_friend.username} </span>
         </div>
-        ))}
-      </div>
-    
+      ))}
+    </div>)
+  :
+  (
+  <span className='noOnlineText'>
+  No Online Friends
+  </span>
+  )
+
   )
 }
 
